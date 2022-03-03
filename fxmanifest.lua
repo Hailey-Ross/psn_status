ce0c736bd98e9a23e3ed8f1d6f1d75e7082beb81
Agent Colorado <26784904+LordBobRoss@users.noreply.github.com>
2022-03-02 17:28:39 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'Original Creator: Darkk Edits by: Hailey'
+author 'Original Creator: Darkk with Edits by: Hailey'
 description 'On Person Status System for RedM'
 
 client_scripts {'client.lua'}
