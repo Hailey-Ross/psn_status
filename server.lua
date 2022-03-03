1e21ea7c8750f2863b629fc41928d724e359f507
Agent Colorado <26784904+LordBobRoss@users.noreply.github.com>
2022-03-02 17:18:53 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,22 +1,22 @@
 local scenes = {}
 
-RegisterServerEvent("darkk_ops:add")
-AddEventHandler("darkk_ops:add", function(text)
+RegisterServerEvent("psn_status:add")
+AddEventHandler("psn_status:add", function(text)
     local _source = source
     local _text = tostring(text)
     local scene = {text = _text, serverid = _source}
     scenes[#scenes+1] = scene
-    TriggerClientEvent("darkk_ops:sendscenes", -1, scenes)
+    TriggerClientEvent("psn_status:sendscenes", -1, scenes)
 end)
 
-RegisterServerEvent("darkk_ops:getscenes")
-AddEventHandler("darkk_ops:getscenes", function(text)
+RegisterServerEvent("psn_status:getscenes")
+AddEventHandler("psn_status:getscenes", function(text)
     local _source = source
-    TriggerClientEvent("darkk_ops:sendscenes", _source, scenes)
+    TriggerClientEvent("psn_status:sendscenes", _source, scenes)
 end)
 
-RegisterServerEvent("darkk_ops:delete")
-AddEventHandler("darkk_ops:delete", function()
+RegisterServerEvent("psn_status:delete")
+AddEventHandler("psn_status:delete", function()
     local _source = source
     if scenes[1] ~= nil then
         for i,v in pairs(scenes) do
@@ -27,5 +27,5 @@ AddEventHandler("darkk_ops:delete", function()
             end
         end
     end
-    TriggerClientEvent("darkk_ops:sendscenes", -1, scenes)
+    TriggerClientEvent("psn_status:sendscenes", -1, scenes)
 end)
